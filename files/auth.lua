f667dde31b2fa508d374b4f2df5b603a62dbee22
songzhu1028 <colin.song@outlook.com>
2014-10-11 13:30:56 +0800

    update auth method

--- files/auth.lua
+++ files/auth.lua
@@ -5,15 +5,19 @@ function index()
 end
 
 function validate()
-	--http://192.168.1.220/cgi-bin/luci/auth/auth/?stage=login&ip=192.168.1.104&mac=50:e5:49:5e:8d:2d&token=f3c58dd37b65f5e2ac8b089685b30d731412653965&incoming=0&outgoing=0&gw_id=080027F9A44E
-	--local tempfile = io.open("/tmp/wifidog-auth/"..token, "w+")
-
+	--http://192.168.1.163/cgi-bin/luci/auth/auth/?stage=login&ip=192.168.1.104&mac=50:e5:49:5e:8d:2d&token=b2aa402babe780a8d4fa4a536f517c68&incoming=0&outgoing=0&gw_id=080027F9A44E
 	local token = luci.http.formvalue("token")
 	if token then
 		local file = io.open("/tmp/wifidog-auth/"..token)
 		if file then
-			os.remove("/tmp/wifidog-auth/"..token)
-			luci.http.write("Auth: 1")
+			local timeauth=file:read("*l")
+			if not timeauth or timeauth-os.time()>3600*2 then
+				file:close()
+				os.remove("/tmp/wifidog-auth/"..token)
+				luci.http.write("Auth: 0")
+			else
+				luci.http.write("Auth: 1")
+			end
 		else
 			luci.http.write("Auth: 0")
 		end
--- files/login.lua
+++ files/login.lua
@@ -5,23 +5,27 @@ function index()
 end
 
 function logging()
-	--http://192.168.1.220/cgi-bin/luci/auth/login/?gw_address=192.168.1.220&gw_port=2060&gw_id=080027F9A44E&mac=50:e5:49:5e:8d:2d&url=http%3A//www.baidu.com/
+	--http://192.168.1.163/cgi-bin/luci/auth/login/?gw_address=192.168.1.163&gw_port=2060&gw_id=080027F9A44E&mac=50:e5:49:5e:8d:2d&url=http%3A//www.baidu.com/
 	local gw_address = luci.http.formvalue("gw_address")
 	local gw_port = luci.http.formvalue("gw_port")
 	local url = luci.http.formvalue("url")
 	if gw_address and gw_port then
 		local token = ""
 		local strlist = {"a","b","c","d","e","f","1","2","3","4","5","6","7","8","9","0"}
+		local strlens=table.getn(strlist)
 		math.randomseed(os.time())
 		for i=1,32,1 do
-			token = token..strlist[math.random(16)]
+			token = token..strlist[math.random(strlens)]
 		end
 		local dir = io.open("/tmp/wifidog-auth")
 		if not dir then
-			luci.fs.mkdir("/tmp/wifidog-auth","rwxrwxrwx")
+			os.execute("mkdir -p /tmp/wifidog-auth")
 		end
-		local file = io.open("/tmp/wifidog-auth/"..token, "w+")
+		local ft = io.open("/tmp/wifidog-auth/"..token, "w+")
+		ft:write(os.time())
+		ft:close()
 		--luci.http.write("http://"..gw_address..":"..gw_port.."/wifidog/auth?token="..token)
+		--luci.http.write("time:"..os.time());
 		luci.http.redirect("http://"..gw_address..":"..gw_port.."/wifidog/auth?token="..token)
 	end
 end
